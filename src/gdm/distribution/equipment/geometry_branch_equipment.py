db2ea5f0098cfc5f1439df64b270cc72f03a266a
Duwadi, Kapil <Kapil.Duwadi@nrel.gov>
2024-03-25 14:29:39 -0500

    Bugfix/branch xy coord (#35)
    * Geometry branch update
    
    - renamed horizontal_spacings to horizontal_positions
    - renamed heights to vertical_positions

--- src/gdm/distribution/equipment/geometry_branch_equipment.py
+++ src/gdm/distribution/equipment/geometry_branch_equipment.py
@@ -1,14 +1,12 @@
 """ This module contains geometry branch equipment."""
 
 from typing import Annotated
-from itertools import groupby
 
 from pydantic import Field, model_validator
 from infrasys.component_models import ComponentWithQuantities
 
 from gdm.distribution.equipment.bare_conductor_equipment import BareConductorEquipment
 from gdm.distribution.equipment.concentric_cable_equipment import ConcentricCableEquipment
-from gdm.distribution.sequence_pair import SequencePair
 from gdm.quantities import Distance
 
 
@@ -19,24 +17,15 @@ class GeometryBranchEquipment(ComponentWithQuantities):
         list[BareConductorEquipment | ConcentricCableEquipment],
         Field(..., description="List of overhead wires or cables."),
     ]
-    spacing_sequences: Annotated[
-        list[SequencePair],
-        Field(
-            ...,
-            description="""List of pair
-            of sequence numbers for coupling """,
-        ),
-    ]
-    horizontal_spacings: Annotated[
+    horizontal_positions: Annotated[
         list[Distance],
-        Field(..., description="Horizontal spacing for each spacing sequences."),
+        Field(..., description="Horizontal position of the conductor."),
     ]
-    heights: Annotated[
+    vertical_positions: Annotated[
         list[Distance],
         Field(
             ...,
-            description="""Heights of each conductor from ground, positive
-            for overhead and negative for underground.""",
+            description="""Vertical position of the conductor.""",
         ),
     ]
 
@@ -47,31 +36,12 @@ class GeometryBranchEquipment(ComponentWithQuantities):
             msg = f"Number of wires must be at least 1 {self.conductors=}"
             raise ValueError(msg)
 
-        if len(self.spacing_sequences) != (len(self.conductors) - 1):
-            msg = (
-                f"Number of spacings {self.spacing_sequences} must be one "
-                f"less tha number of wires {self.conductors=}."
-            )
+        if len(self.horizontal_positions) != len(self.conductors):
+            msg = f"{self.horizontal_positions} and {self.conductors=} must be equal in length."
             raise ValueError(msg)
 
-        for item in self.spacing_sequences:
-            if item.from_index == item.to_index:
-                msg = (
-                    f"From index {item.from_index=} should not be equal "
-                    f"to index {item.to_index}in spacing sequences."
-                )
-                raise ValueError(msg)
-            if item.from_index >= len(self.conductors) or item.to_index >= len(self.conductors):
-                msg = (
-                    f"Sequence index {item=} can not be greater than or equal to"
-                    f"length of conductors {len(self.conductors)}"
-                )
-                raise ValueError(msg)
-
-        if len(list(groupby([set(item) for item in self.spacing_sequences]))) != len(
-            self.spacing_sequences
-        ):
-            msg = f"Invalid sequence numbers in spacing sequences. {self.spacing_sequences=}"
+        if len(self.vertical_positions) != len(self.conductors):
+            msg = f"{self.vertical_positions} and {self.conductors=} must be equal in length."
             raise ValueError(msg)
 
         return self
@@ -82,7 +52,6 @@ class GeometryBranchEquipment(ComponentWithQuantities):
         return GeometryBranchEquipment(
             name="geometry-branch-1",
             conductors=[BareConductorEquipment.example()] * 3,
-            spacing_sequences=[SequencePair(0, 1), SequencePair(1, 2)],
-            horizontal_spacings=[Distance(0, "m")] * 2,
-            heights=[Distance(5.6, "m"), Distance(6.0, "m"), Distance(6.4, "m")],
+            horizontal_positions=[Distance(0, "m")] * 3,
+            vertical_positions=[Distance(5.6, "m"), Distance(6.0, "m"), Distance(6.4, "m")],
         )
