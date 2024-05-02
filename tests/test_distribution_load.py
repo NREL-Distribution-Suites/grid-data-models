dc0b7f334d69ca144cea6b5513a6737fc313f6c8
telgindy <tarek.elgindy@nrel.gov>
2024-05-02 16:33:48 -0600

    adding test for failed ZIP params

---
+++ tests/test_distribution_load.py
@@ -0,0 +1,17 @@
+import pytest
+from gdm import PhaseLoadEquipment, ActivePower, ReactivePower
+
+def test_invalid_power_system_load():
+    with pytest.raises(ValueError) as _:
+        PhaseLoadEquipment(
+            name="phase-1-load-equipment",
+            num_customers=1,
+            real_power = ActivePower(2.5, "kilowatt"),
+            reactive_power = ReactivePower(0, "kilovar"),
+            z_real=0.0,
+            z_imag=0.0,
+            i_real=0.0,
+            i_imag=0.0,
+            p_real=0.0,
+            p_imag=0.0,
+        )
