a3a2d8818014d84c81a77d762ca6cd8d48c3a639
tarekelgindy <tarek.elgindy@gmail.com>
2024-11-11 23:16:06 -0700

    moving conductors to geometry branch rather than equipment

--- src/gdm/distribution/equipment/geometry_branch_equipment.py
+++ src/gdm/distribution/equipment/geometry_branch_equipment.py
@@ -5,8 +5,6 @@ from typing import Annotated
 from pydantic import Field, model_validator
 from infrasys import Component
 
-from gdm.distribution.equipment.bare_conductor_equipment import BareConductorEquipment
-from gdm.distribution.equipment.concentric_cable_equipment import ConcentricCableEquipment
 from gdm.quantities import Distance
 from gdm.constants import PINT_SCHEMA
 
@@ -14,10 +12,6 @@ from gdm.constants import PINT_SCHEMA
 class GeometryBranchEquipment(Component):
     """Interface for geometry branch info."""
 
-    conductors: Annotated[
-        list[BareConductorEquipment | ConcentricCableEquipment],
-        Field(..., description="List of overhead wires or cables."),
-    ]
     horizontal_positions: Annotated[
         Distance,
         PINT_SCHEMA,
@@ -35,18 +29,6 @@ class GeometryBranchEquipment(Component):
     @model_validator(mode="after")
     def validate_fields(self) -> "GeometryBranchEquipment":
         """Custom validator for geometry branch model fields."""
-        if not self.conductors:
-            msg = f"Number of wires must be at least 1 {self.conductors=}"
-            raise ValueError(msg)
-
-        if len(self.horizontal_positions) != len(self.conductors):
-            msg = f"{self.horizontal_positions} and {self.conductors=} must be equal in length."
-            raise ValueError(msg)
-
-        if len(self.vertical_positions) != len(self.conductors):
-            msg = f"{self.vertical_positions} and {self.conductors=} must be equal in length."
-            raise ValueError(msg)
-
         return self
 
     @classmethod
@@ -54,7 +36,6 @@ class GeometryBranchEquipment(Component):
         """Example for geometry branch equipment."""
         return GeometryBranchEquipment(
             name="geometry-branch-1",
-            conductors=[BareConductorEquipment.example()] * 3,
             horizontal_positions=Distance([5.6, 6.0, 6.4], "m") * 3,
             vertical_positions=Distance([5.6, 6.0, 6.4], "m"),
         )
