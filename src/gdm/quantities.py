19e0703a63ead39d313e6da22348aeb6d53f9ee9
Latif <aadil.latif@gmail.com>
2024-11-27 11:03:33 -0700

    refactor

--- src/gdm/quantities.py
+++ src/gdm/quantities.py
@@ -133,6 +133,7 @@ class PositiveReactivePower(ReactivePower):
             np.array(value).flatten() >= 0
         ), f"Reactive power ({value}, {units}) must be positive."
 
+
 class ApparentPower(BaseQuantity):
     """Quantity representing apparent power."""
 
