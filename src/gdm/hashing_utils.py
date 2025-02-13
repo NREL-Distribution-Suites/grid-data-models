8e44e5dd3ffba22f478bfeb2ca115a3fca0bef48
Latif <alatif@nrel.gov>
2025-02-13 09:48:18 -0700

    Update hashing_utils.py

--- src/gdm/hashing_utils.py
+++ src/gdm/hashing_utils.py
@@ -1,33 +1,33 @@
 from infrasys import Component
 
-def _remove_keys_from_dict(
-        model_dict: dict, key_names: list[str] = ["name", "uuid"]
-    ) -> dict:
-        """Method recursively removes keys from the model
 
-        Args:
-            model_dict (dict): model in dict representation
-            key_names (list[str]): keys to remove from the model
+def _remove_keys_from_dict(model_dict: dict, key_names: list[str] = ["name", "uuid"]) -> dict:
+    """Method recursively removes keys from the model
 
-        Returns:
-            dict: reduced model dictionary
-        """
-        for key_name in key_names:
-            if key_name in model_dict:
-                model_dict.pop(key_name)
-            for k, v in model_dict.items():
-                if isinstance(v, dict):
-                    model_dict[k] = _remove_keys_from_dict(v)
-                elif isinstance(v, list):
-                    values = []
-                    for value in v:
-                        if isinstance(value, dict):
-                            value = _remove_keys_from_dict(value)
-                        values.append(value)
-                        model_dict[k] = values
-        return model_dict
+    Args:
+        model_dict (dict): model in dict representation
+        key_names (list[str]): keys to remove from the model
 
-def hash_model( model: Component, key_names: list[str] = ["name", "uuid"]) -> int:
+    Returns:
+        dict: reduced model dictionary
+    """
+    for key_name in key_names:
+        if key_name in model_dict:
+            model_dict.pop(key_name)
+        for k, v in model_dict.items():
+            if isinstance(v, dict):
+                model_dict[k] = _remove_keys_from_dict(v)
+            elif isinstance(v, list):
+                values = []
+                for value in v:
+                    if isinstance(value, dict):
+                        value = _remove_keys_from_dict(value)
+                    values.append(value)
+                    model_dict[k] = values
+    return model_dict
+
+
+def hash_model(model: Component, key_names: list[str] = ["name", "uuid"]) -> int:
     """Return hash of the passed model
 
     Args:
